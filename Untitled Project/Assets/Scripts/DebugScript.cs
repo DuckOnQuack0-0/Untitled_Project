a788e44fa8760f5c3a5160cbb3465ea1f4b003f4
DuckOnQuack0-0 <jjtrinidad1997@gmail.com>
2020-01-05 22:22:24 -0500

    Update ProBuilder
    Updated ProBuilder to work with current version of Unity. Working on creating debug script to avoid certain situautions.

---
+++ Untitled Project/Assets/Scripts/DebugScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    public Vector3 levelStartLocation;
+    private Transform startObject;
+
+    private void Awake()
+    {
+        // Finding the transform of a gameobject via script in the scene
+        startObject = FindObjectOfType<StartObject>().transform;
+    }
+
+    private void Start()
+    {
+        // Visual representation of the position of the starting location on the inspector
+        levelStartLocation = startObject.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            print("Button has been pressed!");
+            this.transform.position = levelStartLocation;
+        }
+    }
+
+
+}
