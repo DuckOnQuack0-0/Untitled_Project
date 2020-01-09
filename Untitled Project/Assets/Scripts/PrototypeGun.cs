d2e3b37b3229b65b3c3a90554961e6f51f0b3ec4
DuckOnQuack0-0 <jjtrinidad1997@gmail.com>
2020-01-09 12:52:09 -0500

    Added Weapon Function
    Added raycast to the prototype weapon
    
    Working on target script

--- Untitled Project/Assets/Scripts/PrototypeGun.cs
+++ Untitled Project/Assets/Scripts/PrototypeGun.cs
@@ -1,18 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PrototypeGun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject gunBarrel;
+    public Camera mainCam;
+
+    [SerializeField] private float range = 100f;
+    public float damage = 10f;
+
+    [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float nextTimeToFire = 0f;
+
+    public GameObject bangStandin;
+
+
+    private void Awake()
     {
-        
+        bangStandin.SetActive(false);
+        gunBarrel = GameObject.FindGameObjectWithTag("PrototypeBarrel");
+        mainCam = Camera.FindObjectOfType<PlayerLook>().GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            FireWeapon();
+            bangStandin.SetActive(true);
+            if(bangStandin)
+            print("Shot Fired!");
+        }
+        else
+        {
+            bangStandin.SetActive(false);
+        }
+    }
+
+    private void FireWeapon()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.DrawRay(mainCam.transform.position, mainCam.transform.forward, Color.cyan);
+
+           Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.Damage(damage);
+            }
+        }
+            
     }
 }
---
+++ Untitled Project/Assets/Scripts/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private float targetHealth = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(float amount)
+    {
+        targetHealth -= amount;
+        if(targetHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
