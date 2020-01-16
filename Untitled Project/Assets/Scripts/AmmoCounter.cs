8f0c0dc1530cfbfd2ae470485fe3965b94cab54b
DuckOnQuack0-0 <jjtrinidad1997@gmail.com>
2020-01-16 15:05:23 -0500

    Added Bullet Impact
    -Added bullet impact effect
    -Added UI Ammo counter
    -Created new script so the ammo UI can be updated
    -Added new jumping area for the player to practice jumping mechanic

---
+++ Untitled Project/Assets/Scripts/AmmoCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCounter : MonoBehaviour
+{
+    public Text ammoLeftText;
+    public Text ammoCapacityText;
+    public float ammoLeftAmount;
+    private float ammoCapacityAmount = 17f;
+    [SerializeField] private PrototypeGun prototypeGunScript;
+
+    private void Awake()
+    {
+        //Seeking the prototypegunscript within the active scene
+        prototypeGunScript = FindObjectOfType<PrototypeGun>();
+        //Setting the ammo capacity text to the guns ammo capactiy
+        ammoCapacityText.text = ammoCapacityAmount.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Setting the float within this script to equal the bullet amount left in the prototype gun script
+        ammoLeftAmount = prototypeGunScript.bulletsLeft;
+        //Setting the ammoleftText to the text and so it can be updated every frame
+        ammoLeftText.text = ammoLeftAmount.ToString();
+    }
+}
--- Untitled Project/Assets/Scripts/PrototypeGun.cs
+++ Untitled Project/Assets/Scripts/PrototypeGun.cs
@@ -8,39 +8,82 @@ public class PrototypeGun : MonoBehaviour
 
     public float range = 100f;
     public float damage = 10f;
+    [SerializeField]private float maxAmmo = 85f;
 
-    [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float fireRate = 20f;
     [SerializeField] private float nextTimeToFire = 0f;
+    public int bulletsLeft = 17;
 
-    public GameObject bangStandin;
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private ParticleSystem muzzleParticles;
+    
     public Image crosshair;
     private AudioSource gunShot;
+    public GameObject reloadText;
+
+    [SerializeField] private bool needsToReload = false;
+    public bool firedGun = false;
+
+    public GameObject impactParticle;
+
 
 
     private void Awake()
     {
-        bangStandin.SetActive(false);
+        #region TempCode
+        /*Using Temperary UI for the muzzle flash 
+        bangStandin.SetActive(false);*/
+        #endregion
+        //Finding the game object in the heirarchy via Tag 
         gunBarrel = GameObject.FindGameObjectWithTag("PrototypeBarrel");
+        //Finding the camera via component on to where the player look script is located
         mainCam = Camera.FindObjectOfType<PlayerLook>().GetComponent<Camera>();
+        //Finding the gunShot audio source attached to the gameobject 
         gunShot = GetComponent<AudioSource>();
+        //GameObject containing the muzzlefalsh particles
+        muzzleFlash = GameObject.FindGameObjectWithTag("PrototypeMuzzleFlash");
+        //The particle system component with in the muzzleFlash gameObject 
+        muzzleParticles = muzzleFlash.GetComponent<ParticleSystem>();
+        //Setting the text inactive until called later in the script 
+        reloadText.SetActive(false);
+        #region Test Animator Code
+        //Getting the animatior component
+        //animator = GetComponent<Animator>();
+        #endregion
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && needsToReload == false && bulletsLeft > 0)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
             FireWeapon();
+            firedGun = true;
+            muzzleParticles.Play();
             gunShot.Play();
-            bangStandin.SetActive(true);
+            bulletsLeft -= 1;
+            //maxAmmo -= 1f;
+            #region TestCode
+            /*bangStandin.SetActive(true);
             if (bangStandin)
-                print("Shot Fired!");
+                print("Shot Fired!");*/
+            #endregion
         }
-        else
+
+        if (bulletsLeft == 0)
         {
-            bangStandin.SetActive(false);
+            needsToReload = true;
+            reloadText.SetActive(true);
         }
+        else if(bulletsLeft > 0)
+        {
+            needsToReload = false;
+            reloadText.SetActive(false);
+        }
+
+        Reload();
 
         RaycastHit hit;
 
@@ -48,6 +91,8 @@ public class PrototypeGun : MonoBehaviour
         {
             Debug.DrawRay(mainCam.transform.position, mainCam.transform.forward * range, Color.cyan);
             Target target = hit.transform.GetComponent<Target>();
+            
+
             if(target != null)
             {
                 crosshair.color = Color.red;
@@ -73,7 +118,20 @@ public class PrototypeGun : MonoBehaviour
             {
                 target.Damage(damage);
             }
+
+            GameObject impactGO = Instantiate(impactParticle, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+
         }
             
+    }
+
+    private void Reload()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            bulletsLeft = 17;
+        } 
+        
     }
 }
