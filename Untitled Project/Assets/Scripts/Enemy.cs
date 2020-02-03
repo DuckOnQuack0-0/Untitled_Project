a052dc76a8d2f5e9cba0ddbd0c72d8bd4ea1e8b6
Mattmatt04 <60557314+Mattmatt04@users.noreply.github.com>
2020-02-02 19:31:15 -0500

    Enemies That Follow/Retreat/Shoot
    Not Done Yet

---
+++ Untitled Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    [SerializeField] private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        } else if (Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        } else if (Vector3.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -moveSpeed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        } else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
