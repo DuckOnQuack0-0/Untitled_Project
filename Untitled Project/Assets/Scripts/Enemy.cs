1d1b76d7f34a74ba9a76d839c50197ac14edbf08
Mattmatt04 <60557314+Mattmatt04@users.noreply.github.com>
2020-02-08 12:05:00 -0500

    Better Enemy
    I scrapped the last thing, using Brackey's rpg tutorial to make this now. Not done yet.

--- Untitled Project/Assets/Scripts/Enemy.cs
+++ Untitled Project/Assets/Scripts/Enemy.cs
@@ -2,26 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Interactable
 {
-
-    public float moveSpeed;
-    public float stoppingDistance;
-    public float retreatDistance;
-
-    [SerializeField] private float timeBtwShots;
-    public float startTimeBtwShots;
-
-    public GameObject projectile;
-    public Transform player;
-
-    private void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        timeBtwShots = startTimeBtwShots;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -31,25 +13,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.position) > stoppingDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-        } else if (Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
-        {
-            transform.position = this.transform.position;
-        } else if (Vector3.Distance(transform.position, player.position) < retreatDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, -moveSpeed * Time.deltaTime);
-        }
-
-        if (timeBtwShots <= 0)
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
-
-        } else
-        {
-            timeBtwShots -= Time.deltaTime;
-        }
+        
     }
 }
